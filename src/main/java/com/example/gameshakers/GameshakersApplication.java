1ec5f44e4b5159d8e96d584b3aa3cfb3a8822c48
kemboi22 <kemboielvis22@gmail.xom>
2023-09-25 13:10:43 +0300

    Initial COmmit

---
+++ src/main/java/com/example/gameshakers/GameshakersApplication.java
@@ -0,0 +1,13 @@
+package com.example.gameshakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameshakersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameshakersApplication.class, args);
+    }
+
+}
